af737ace9422c28c2754a46f9331152bb2eb4060
Bansal <Saransh.Bansal@gartner.com>
2018-04-10 15:35:21 +0530

    integrated bootstrap 4 with angular 5.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
+import { NgbdAlertCloseable } from '../ui-library/ng-alert/alert-closeable.component';
 
-
+/* Modules are used in Angular JS to put logical boundaries in your application. Hence, instead of coding everything into one application, you can instead build everything into separate modules to separate the functionality of your application. */
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgbdAlertCloseable
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
